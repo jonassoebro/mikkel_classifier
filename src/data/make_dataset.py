807e24a6c06a9c955560330e6a5f66205ddbc3aa
jonas <jonassoebro@gmail.com>
2022-06-05 15:02:42 +0200

    updated dvc stuff

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import os
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+from process_raw_image import process_raw_image
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,15 +15,27 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
-
+    
+    # Make target dirs if they do not already exists
+    if not os.path.isdir("data/processed/negative"):
+        os.mkdir("data/processed/negative")
+    if not os.path.isdir("data/processed/positive"):
+        os.mkdir("data/processed/positive")
+    
+    # Rename images as numbers
+    counter = 0
+    for img_name in os.listdir(input_filepath):
+        img_path = os.path.join(input_filepath, img_name)
+        img_name = counter
+        process_raw_image(img_path, output_filepath, output_imgname=counter, imgsize=64)
+        counter = counter+1
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
+    
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
---
+++ src/data/process_raw_image.py
@@ -0,0 +1,32 @@
+from importlib.resources import path
+from PIL import Image
+
+# function for resizing and cropping to 64x64
+def process_raw_image(input_imgpath: path, output_imgpath: path, output_imgname: str, imgsize: int=64) -> object:
+    '''Input a path to a png-image and output a cropped and resized square image saved in a desired folder.'''
+    
+    im = Image.open(input_imgpath)
+
+    # Get size
+    x, y = im.size
+
+    # New sizes
+    yNew = imgsize
+    xNew = yNew 
+
+    # First, set right size
+    if x > y:
+        # Y is smallest, figure out relation to 256
+        xNew = round(x * imgsize / y)
+    else:
+        yNew = round(y * imgsize / x)
+
+    # resize
+    im.resize((int(xNew), int(yNew)), Image.ANTIALIAS)
+
+    # crop
+    im.crop(((int(xNew) - imgsize)/2, (int(yNew) - imgsize)/2, (int(xNew) + imgsize)/2, (int(yNew) + imgsize)/2))
+
+    # save
+    print(f"Saving image in {output_imgpath}")
+    im.save(output_imgpath+f"{output_imgname}.png")
